6905489fe088d14a2af9892469d065282748737b
Sam Kettle <156723890+s-kettle@users.noreply.github.com>
2024-03-01 11:30:34 +0000

    Add comment to main.py

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import requests
 from requests.exceptions import HTTPError
 
 try:
+    # Retrieve artist and song title information from user
     artist = input("Enter an artist: ")
     title = input("Enter a song title: ")
 
+    # Get a response from the lyrics.ovh API
     response = requests.get(f'https://api.lyrics.ovh/v1/{artist}/{title}')
     response.raise_for_status()
 
+    # Parse the response to JSON
     json = response.json()
 
+    # Print the lyrics to the console
     print(f"Here are the lyrics for {title} by {artist}")
     print(json["lyrics"])
 
